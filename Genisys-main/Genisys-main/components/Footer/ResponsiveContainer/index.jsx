876948beed29cdb8094b69002d79cf4cab616d86
dracocodess <138291073+dracocodess@users.noreply.github.com>
2023-07-08 22:39:56 +0530

    Delete Genisys-main directory

--- Genisys-main/Genisys-main/components/Footer/ResponsiveContainer/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Container } from '@chakra-ui/react';
-
-const ResponsiveContainer = ({ children }) => (
-  <Container maxW={['95%', '90%', '85%', '90%', '85%', '70%']}>
-    {children}
-  </Container>
-);
-
-export default ResponsiveContainer;
