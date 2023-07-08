d0af271154ee6000ba726518d592b88f13af12c6
dracocodess <138291073+dracocodess@users.noreply.github.com>
2023-07-08 22:42:16 +0530

    Delete Genisys-main directory

--- Genisys-main/pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css'
-import '../styles/timeline.scss'
-import '../styles/Home.module.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
