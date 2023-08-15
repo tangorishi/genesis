dc657ddeb6b2563f9db8c4c648652f6bc6f6cd00
Rishi Joshi <110519406+tangorishi@users.noreply.github.com>
2023-08-16 04:20:48 +0530

    Update index.jsx

--- components/Timeline/index.jsx
+++ components/Timeline/index.jsx
@@ -3,27 +3,35 @@ import { Box, SimpleGrid } from "@chakra-ui/react";
 import Card from "../Card";
 import events from "./events";
 import FadeInUp from "../Anim/FadeInUp";
+
 const Timeline = () => {
   return (
-    <Box px={10} mx={6}>
+    <Box px={10} mx={6} my={10}>
       <FadeInUp>
-        <SimpleGrid columns={[1, 1, 1, 2, 3]} spacingX={"1.2rem"} px={"13vw"}>
+        <SimpleGrid columns={[1, 1, 1, 2, 3]} spacingX={"1.2rem"} px={"11vw"}>
           {events.map((res) => (
-            <Card
+            <Box
               key={res.key}
-              image={res["image"]}
-              type={res["type"]}
-              title={res["title"]}
-              desc={res["desc"]}
-              auth_name={res["auth_name"]}
-              auth_img={res["auth_img"]}
-              link={res["link"]}
-              tags={res["tags"]}
-              linkGH={res["linkGH"]}
-              status={res["status"]}
-              date={res["date"]}
-              form={res["form"]}
-            />
+              transition="box-shadow 0.3s"
+              _hover={{
+                boxShadow: "0px 8px 160px 0px rgba(0,0,0,1)",
+              }}
+            >
+              <Card
+                image={res["image"]}
+                type={res["type"]}
+                title={res["title"]}
+                desc={res["desc"]}
+                auth_name={res["auth_name"]}
+                auth_img={res["auth_img"]}
+                link={res["link"]}
+                tags={res["tags"]}
+                linkGH={res["linkGH"]}
+                status={res["status"]}
+                date={res["date"]}
+                form={res["form"]}
+              />
+            </Box>
           ))}
         </SimpleGrid>
       </FadeInUp>
@@ -32,3 +40,4 @@ const Timeline = () => {
 };
 
 export default Timeline;
+
